8a2a6e683b3468c66808c69a385e8d39993488a3
ihdhdvdjhd1864-lab <ihdhdvdjhd1864@gmail.com>
2026-03-25 01:02:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+document.querySelectorAll(".hidden").forEach((el) => {
+  observer.observe(el);
+});
+
+
+
+
+
+
+
+
+
