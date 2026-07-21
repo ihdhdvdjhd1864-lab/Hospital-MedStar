cafd9e897a60223817513c473308275ea6b03a28
Karim Mohamed <ihdhdvdjhd1864@gmail.com>
2026-07-21 04:40:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-      observer.unobserve(entry.target);
-    }
-  });
-}, {
-  threshold: 0.2
-});
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
 
 document.querySelectorAll(".hidden").forEach((el) => {
   observer.observe(el);
 });
 
+let menuBtn = document.querySelector(".menu-btn");
+let navLinks = document.querySelector(".nav1");
 
+menuBtn.onclick = function () {
+  navLinks.classList.toggle("active");
 
-
-
-
-
-
-
+  if (navLinks.classList.contains("active")) {
+    menuBtn.innerHTML = "✕";
+  } else {
+    menuBtn.innerHTML = "☰";
+  }
+};
